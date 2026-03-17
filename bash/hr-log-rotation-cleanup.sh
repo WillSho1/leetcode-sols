700e87af8dc539c2355cce7f5aff08349907ec86
Will (via Jass) <will@example.com>
2026-03-17 14:19:37 +0000

    SWAP: Replace complex log rotation with HackerRank Grep #3

--- bash/hr-log-rotation-cleanup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# hr-log-rotation-cleanup.sh
-# Goal: Script to rotate logs and cleanup files based on retention.
-# Reference: https://www.linuxteck.com/linux-shell-scripting-interview-questions/
